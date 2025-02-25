305c752acf1e7d3246e6904b87c382f5883603c9
pranavpallattu <pranavpspallattu@gmail.com>
2025-02-25 11:05:33 +0530

    port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const router = jsonServer.router('db.json')
 libraryManage.use(middleware)
 libraryManage.use(router)
 
-const PORT = 4000 || process.env.PORT
+const PORT =  process.env.PORT || 4000
 
 libraryManage.listen(PORT,()=>{
     console.log(`Server running successful at port no: ${PORT}`);
